e575b853f28f6687a4cf11b16e652b426f033dca
Jordan Mackie <jm354@st-andrews.ac.uk>
2018-10-31 16:37:40 +0000

    Found easier bandpass filter. Will need to look into how it works but noticed smoothing which my implementation lacks

--- filterSubBand.m
+++
@@ -1,7 +0,0 @@
-% Custom filter function, removes all frequencies inside of the given band
-function fourierTransform = filterSubBand(fourierTransform, frequencyAxis, lowerRange, upperRange)
-    frequencyAxis(frequencyAxis >= lowerRange & frequencyAxis <= upperRange) = 0;
-    frequencyAxis(frequencyAxis <= -lowerRange & frequencyAxis >= -upperRange) = 0; % Imaginary values? -_-
-    frequencyAxis(frequencyAxis ~= 0) = 1;
-    fourierTransform = fourierTransform .* frequencyAxis';
-end
